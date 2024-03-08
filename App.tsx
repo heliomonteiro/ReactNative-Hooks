7764e1d2b8f0c0f768788cebcee92981451275fc
Helio Monteiro <heliojr.monteiro@yahoo.com.br>
2024-03-07 22:25:20 -0300

    preparacao ambiente + emulador + criacao do projeto

---
+++ App.tsx
@@ -0,0 +1,19 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+
+import { SafeAreaView, Text } from 'react-native';
+
+
+function App() {
+  return <SafeAreaView>
+    <Text>Alura</Text>
+  </SafeAreaView>
+}
+
+export default App;
